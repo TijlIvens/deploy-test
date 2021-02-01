20ee5279448bc097dd628cb5123c426ae5fd956c
Tijl <tijl.ivens@gmail.com>
2021-02-01 13:03:41 +0100

    1.0.5

--- release.js
+++ release.js
@@ -1,5 +1,29 @@
 module.exports = async (markdown, metaData) => {
-  var pjson = require("./package.json");
-  console.log(pjson.version);
+  const fs = require("fs");
+
+  const { version } = require("./package.json");
+
+  var project1Json = require("./packages/project1/package.json");
+  var project2Json = require("./packages/project2/package.json");
+
+  project1Json.version = version;
+  project2Json.version = version;
+
+  fs.writeFile(
+    "./packages/project1/package.json",
+    JSON.stringify(project1Json, null, 2),
+    (err) => {
+      if (err) return console.log(err);
+    }
+  );
+
+  fs.writeFile(
+    "./packages/project2/package.json",
+    JSON.stringify(project2Json, null, 2),
+    (err) => {
+      if (err) return console.log(err);
+    }
+  );
+
   return markdown;
 };
